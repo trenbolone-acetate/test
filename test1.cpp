f1b4c018e7555008f6279016da0907acfec86ed6
Артем <cheezballz03@gmail.com>
2022-11-10 14:02:31 +0200

    pasted lb №5

--- test1.cpp
+++ test1.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
-
-int main(){
-	int second_commit;
-	cin >> second_commit;
-	cout << second_commit * 2;
-
+int main()
+{
+	int a, b, c, x;
+	double F;
+	cout << "Input a: ";
+	cin >> a;
+	cout << "Input b: ";
+	cin >> b;
+	cout << "Input c: ";
+	cin >> c;
+	cout << "Input x: ";
+	cin >> x;
+	if (c < 0 || x != 0) {
+		F = -a * x - c;
+	}
+	else if (c > 0 && x == 0) {
+		F = (static_cast<float>(x) - a) / -c;
+	}
+	else {
+		F = (b * x) / (c - a);
+	}
+	cout << "F equals to: " << F;
 }
